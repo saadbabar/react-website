33aa0eb92b83723c9df90c10763c75183bdd42fb
Saad Babar <saadmb@umich.edu>
2024-09-23 22:45:46 -0400

    web vitals

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -25,3 +25,5 @@ export function Experience() {
     </section>
   );
 }
+
+export default Experience;
--- my-app/src/reportWebVitals.ts
+++ my-app/src/reportWebVitals.ts
@@ -1,14 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+// Comment out or remove this line
+// import { ReportHandler } from 'web-vitals';
 
 const reportWebVitals = (onPerfEntry?: ReportHandler) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+    // Comment out or remove the import('web-vitals') call
+    // import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+    //   getCLS(onPerfEntry);
+    //   getFID(onPerfEntry);
+    //   getFCP(onPerfEntry);
+    //   getLCP(onPerfEntry);
+    //   getTTFB(onPerfEntry);
+    // });
   }
 };
 
