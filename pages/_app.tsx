3feba4c22d2b0221e03e86e041f6e90c3738badb
Saad Babar <saadmb@umich.edu>
2024-09-24 18:50:14 -0400

    added google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,32 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script'; // Import Script from Next.js
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Google Analytics Tag */}
+      <Script
+        strategy="afterInteractive" // Ensure the script loads after the page is interactive
+        src={`https://www.googletagmanager.com/gtag/js?id=G-1S41ND413L`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive" // Same as above
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-1S41ND413L', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
