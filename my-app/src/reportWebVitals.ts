270936cf851725baf61c47b9aebc519c922ec6a4
Saad Babar <saadmb@umich.edu>
2024-09-23 22:48:47 -0400

    web

--- my-app/src/reportWebVitals.ts
+++ my-app/src/reportWebVitals.ts
@@ -1,17 +1,8 @@
 // Comment out or remove this line
 // import { ReportHandler } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    // Comment out or remove the import('web-vitals') call
-    // import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-    //   getCLS(onPerfEntry);
-    //   getFID(onPerfEntry);
-    //   getFCP(onPerfEntry);
-    //   getLCP(onPerfEntry);
-    //   getTTFB(onPerfEntry);
-    // });
-  }
+const reportWebVitals = (onPerfEntry?: any) => {
+  // Function body intentionally left empty
 };
 
 export default reportWebVitals;
